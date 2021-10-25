11bd809111e3e1ce67300dfda46473a2e49a443e
Dzeratsu <a498008@yandex.ru>
2021-10-25 17:22:50 +0400

    repair

--- pages/payment.vue
+++ pages/payment.vue
@@ -1,19 +1,41 @@
 <template>
-<div class="container">
+<div class="container center">
+  <h1 v-if="message">{{message}}</h1>
+  {{ford3dData}}
   <form id="paymentFormSample" onsubmit="return false;">
     <input type="text" data-cp="cardNumber" value="4242 4242 4242 4242">
     <input type="text" data-cp="expDateMonth" value="05">
     <input type="text" data-cp="expDateYear" value="22">
     <input type="text" data-cp="cvv" value="633">
-    <input type="text" data-cp="name" value="KONSTANTIN TESTOV">
+    <input type="text" data-cp="name" value="KONSTANTIN TESTOV" v-model="userName">
     <button id="clickmy" type="submit" @click="submit">Оплатить 100 р.</button>
+  </form>
+  <form name="downloadForm" action="https://demo.cloudpayments.ru/acs" method="POST"  v-show="form3d" ref="sumb">
+    <input type="hidden" name="PaReq" :value="ford3dData.PaReq">
+    <input type="hidden" name="MD" :value="ford3dData.MD">
+    <input type="hidden" name="TermUrl" :value="ford3dData.TermUrl">
   </form>
 </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "payment",
+  data() {
+    return {
+      form3d: false,
+      userName: '',
+      key: '',
+      ford3dData: {
+        PaReq: '',
+        MD: '',
+        TermUrl: ''
+      },
+      message: ''
+    }
+  },
   mounted() {
   },
   methods:{
@@ -25,8 +47,8 @@ export default {
         document.getElementById("paymentFormSample"));
       var result = checkout.createCryptogramPacket();
       if (result.success) {
-        // сформирована криптограмма
-        //alert(result.packet);
+        this.key = result.packet
+        this.submitData()
         console.log(result.packet);
       }
       else {
@@ -37,11 +59,37 @@ export default {
           alert(result.messages[msgName]);
         }
       }
+    },
+    submitData() {
+      let object = {
+        "packet": `${this.key}`,
+        "name": `${this.userName}`
+      }
+      axios.post(`https://api.hamiliya.space/cloudpayments/payments/cards/charge`, object,{headers: {Authorization: `Bearer ${localStorage.getItem('token')}`}})
+      .then((res) => {
+        if(typeof(res.data.result) == 'object'){
+          this.ford3dData.PaReq = res.data.result.PaReq
+          this.ford3dData.MD = res.data.result.MD
+          this.ford3dData.TermUrl = res.data.result.TermUrl
+          setTimeout(() => this.$refs.sumb.submit(), 1000 )
+      }else{
+          if(res.data.result){
+          this.message = res.data.result
+          }else {
+            this.message = res.data.errors
+          }
+        }
+      })
     }
   }
 }
 </script>
 
 <style scoped>
-
+h1{
+  font-size: 25px;
+}
+.center {
+  text-align: center;
+}
 </style>
